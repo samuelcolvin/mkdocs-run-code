251f9d633a271581c4da2b53970c1326bb161786
Samuel Colvin <s@muelcolvin.com>
2023-09-06 10:15:59 +0100

    formatting

--- src/main.ts
+++ src/main.ts
@@ -1,67 +1,68 @@
-import {EditorView, basicSetup} from 'codemirror'
+import { EditorView, basicSetup } from 'codemirror'
 import { indentUnit } from '@codemirror/language'
-import {python} from '@codemirror/lang-python'
+import { python } from '@codemirror/lang-python'
 import Convert from 'ansi-to-html'
 import './run_code.css'
 
 function load_css() {
-  const head = document.head;
-  const link = document.createElement('link');
-  link.type = 'text/css';
-  link.rel = 'stylesheet';
-  const srcEl: HTMLScriptElement | null = document.querySelector('script[src*="run_code_main.js"]')
+  const head = document.head
+  const link = document.createElement('link')
+  link.type = 'text/css'
+  link.rel = 'stylesheet'
+  const srcEl: HTMLScriptElement | null = document.querySelector(
+    'script[src*="run_code_main.js"]',
+  )
   if (srcEl) {
     const srcUrl = srcEl.src
-    link.href = srcUrl.replace(/\.js$/, '.min.css');
-    head.appendChild(link);
+    link.href = srcUrl.replace(/\.js$/, '.min.css')
+    head.appendChild(link)
   } else {
     throw new Error('could not find script tag for `run_code_main.js`.')
   }
 }
 load_css()
 
-
 document.querySelectorAll('.language-py pre').forEach((block) => {
   const btn = document.createElement('button')
   // console.log('adding code to:', block)
   btn.className = 'run-code'
   btn.addEventListener('click', (e) => {
-    const target = e.target as HTMLElement | null;
+    const target = e.target as HTMLElement | null
     if (target?.parentNode) {
-      run_block(target.parentNode as HTMLElement)
+      run_block(target.parentNode)
     }
   })
   block.appendChild(btn)
 })
 
-let terminal_output = '';
+let terminal_output = ''
 
-const query_args = new URLSearchParams(location.search);
-query_args.set('ts', Date.now().toString());
+const query_args = new URLSearchParams(location.search)
+query_args.set('ts', Date.now().toString())
 
 let output_el: HTMLElement | null = null
 const ansi_converter = new Convert()
-const decoder = new TextDecoder();
+const decoder = new TextDecoder()
 
-const worker = new Worker(`./dist/run_code_worker.js?${query_args.toString()}`);
-worker.onmessage = ({data}) => {
+const worker = new Worker(`./dist/run_code_worker.js?${query_args.toString()}`)
+worker.onmessage = ({ data }) => {
   if (typeof data == 'string') {
-    terminal_output += data;
+    terminal_output += data
   } else {
     for (const chunk of data) {
-      const arr = new Uint8Array(chunk);
-      const extra = decoder.decode(arr);
-      terminal_output += extra;
+      const arr = new Uint8Array(chunk)
+      const extra = decoder.decode(arr)
+      terminal_output += extra
     }
   }
   if (output_el) {
-    output_el.innerHTML = ansi_converter.toHtml(terminal_output);
+    output_el.innerHTML = ansi_converter.toHtml(terminal_output)
     // scrolls to the bottom of the div
-    output_el.scrollIntoView(false);
+    output_el.scrollIntoView(false)
   }
 }
 
-function run_block(block_root: HTMLElement) {
+function run_block(block_root: ParentNode) {
   const cmElement = block_root.querySelector('.cm-content')
   let python_code
   if (cmElement) {
@@ -77,17 +78,17 @@ function run_block(block_root: HTMLElement) {
 
     new EditorView({
       extensions: [basicSetup, python(), indentUnit.of('    ')],
-      parent: block_root,
+      parent: block_root as HTMLElement,
       doc: python_code,
     })
   }
 
-  terminal_output = '';
-  output_el = document.getElementById('output');
+  terminal_output = ''
+  output_el = document.getElementById('output')
   if (!output_el) {
     throw new Error('could not find `#output` element')
   }
-  output_el.innerText = 'Starting Python and installing dependencies...';
+  output_el.innerText = 'Starting Python and installing dependencies...'
   python_code = python_code.replace(new RegExp(`^ {8}`, 'gm'), '')
-  worker.postMessage(python_code);
+  worker.postMessage(python_code)
 }
--- src/worker.js
+++ src/worker.js
@@ -1,61 +1,61 @@
-let chunks = [];
-let last_post = 0;
+let chunks = []
+let last_post = 0
 
 function print(tty) {
   if (tty.output && tty.output.length > 0) {
-    chunks.push(tty.output);
-    tty.output = [];
-    const now = performance.now();
+    chunks.push(tty.output)
+    tty.output = []
+    const now = performance.now()
     if (now - last_post > 100) {
-      post();
-      last_post = now;
+      post()
+      last_post = now
     }
   }
 }
 
 function post() {
-  self.postMessage(chunks);
-  chunks = [];
+  self.postMessage(chunks)
+  chunks = []
 }
 
 function make_tty_ops() {
   return {
     put_char(tty, val) {
       if (val !== null) {
-        tty.output.push(val);
+        tty.output.push(val)
       }
       if (val === null || val === 10) {
-        print(tty);
+        print(tty)
       }
     },
     fsync(tty) {
-      print(tty);
+      print(tty)
     },
-  };
+  }
 }
 
 function setupStreams(FS, TTY) {
-  let mytty = FS.makedev(FS.createDevice.major++, 0);
-  let myttyerr = FS.makedev(FS.createDevice.major++, 0);
-  TTY.register(mytty, make_tty_ops());
-  TTY.register(myttyerr, make_tty_ops());
-  FS.mkdev('/dev/mytty', mytty);
-  FS.mkdev('/dev/myttyerr', myttyerr);
-  FS.unlink('/dev/stdin');
-  FS.unlink('/dev/stdout');
-  FS.unlink('/dev/stderr');
-  FS.symlink('/dev/mytty', '/dev/stdin');
-  FS.symlink('/dev/mytty', '/dev/stdout');
-  FS.symlink('/dev/myttyerr', '/dev/stderr');
-  FS.closeStream(0);
-  FS.closeStream(1);
-  FS.closeStream(2);
-  FS.open('/dev/stdin', 0);
-  FS.open('/dev/stdout', 1);
-  FS.open('/dev/stderr', 1);
+  let mytty = FS.makedev(FS.createDevice.major++, 0)
+  let myttyerr = FS.makedev(FS.createDevice.major++, 0)
+  TTY.register(mytty, make_tty_ops())
+  TTY.register(myttyerr, make_tty_ops())
+  FS.mkdev('/dev/mytty', mytty)
+  FS.mkdev('/dev/myttyerr', myttyerr)
+  FS.unlink('/dev/stdin')
+  FS.unlink('/dev/stdout')
+  FS.unlink('/dev/stderr')
+  FS.symlink('/dev/mytty', '/dev/stdin')
+  FS.symlink('/dev/mytty', '/dev/stdout')
+  FS.symlink('/dev/myttyerr', '/dev/stderr')
+  FS.closeStream(0)
+  FS.closeStream(1)
+  FS.closeStream(2)
+  FS.open('/dev/stdin', 0)
+  FS.open('/dev/stdout', 1)
+  FS.open('/dev/stderr', 1)
 }
 
-let reformat_exception = null;
+let reformat_exception = null
 
 const log = (msg) => {
   console.debug('log:', msg)
@@ -65,15 +65,17 @@ const log = (msg) => {
 async function load() {
   if (reformat_exception === null) {
     console.debug('Downloading pyodide...')
-    await importScripts('https://cdn.jsdelivr.net/pyodide/v0.23.4/full/pyodide.js');
+    await importScripts(
+      'https://cdn.jsdelivr.net/pyodide/v0.23.4/full/pyodide.js',
+    )
 
     console.debug('Loading pyodide...')
-    pyodide = await loadPyodide();
-    const {FS} = pyodide;
-    setupStreams(FS, pyodide._module.TTY);
+    pyodide = await loadPyodide()
+    const { FS } = pyodide
+    setupStreams(FS, pyodide._module.TTY)
 
     console.debug('Loading micropip...')
-    await pyodide.loadPackage(['micropip']);
+    await pyodide.loadPackage(['micropip'])
     const micropip = pyodide.pyimport('micropip')
 
     const pydantic_core_version = '2.6.3'
@@ -100,9 +102,10 @@ def reformat_exception():
     lines.pop(1)
     lines.pop(1)
     return ''.join(lines)
-`);
+`,
+    )
 
-    reformat_exception = pyodide.globals.get("reformat_exception");
+    reformat_exception = pyodide.globals.get('reformat_exception')
   }
 }
 
@@ -119,11 +122,11 @@ import pydantic, pydantic_core
 print(f'pydantic version: v{pydantic.__version__}, pydantic-core version: v{pydantic_core.__version__}')
 `)
   try {
-    await pyodide.runPythonAsync(event.data);
+    await pyodide.runPythonAsync(event.data)
   } catch (e) {
     post()
     log(reformat_exception())
     return
   }
   post()
-};
+}
